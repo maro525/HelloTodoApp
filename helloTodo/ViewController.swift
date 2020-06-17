edf769a3b62a5d2f537196d4e019c0b662e97cd7
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-06-17 22:23:41 +0900

    Initial Commit

---
+++ helloTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloTodo
+//
+//  Created by Hidemaro Fujinami on 2020/06/17.
+//  Copyright © 2020 maro525. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
